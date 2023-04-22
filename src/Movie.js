43bbfe2b305ea76bcba6db6a32805000c3475b30
Dheeraj62 <56032074+Dheeraj62@users.noreply.github.com>
2023-04-22 22:49:09 +0530

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const Movie = ({ movie }) => {
+  const poster = movie.Poster === 'N/A' ? 'https://via.placeholder.com/300x445' : movie.Poster;
+
   return (
     <div className="movie">
-      <img src={movie.Poster} alt={movie.Title} />
-      <div className="movie-info">
-        <h3>{movie.Title}</h3>
-        <span>{movie.Year}</span>
+      <h2>{movie.Title}</h2>
+      <div>
+        <img src={poster} alt={`The movie titled: ${movie.Title}`} />
       </div>
+      <p>Released: {movie.released}</p>
+      <p>IMDb rating: {movie.imdbRating}</p>
     </div>
   );
 };
+
 
 export default Movie;
